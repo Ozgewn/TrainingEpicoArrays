0369454fbcf9df7c8eeb6f5ea9c14fefb5c7f1eb
Ozgewn <guisodrogadicto289@gmail.com>
2021-03-15 15:00:29 +0100

    primer commit

---
+++ src/EqualsEpico/EqualsEpico.java
@@ -0,0 +1,21 @@
+package EqualsEpico;
+
+public class EqualsEpico {
+	
+	/*@Override
+    public boolean equals(Object obj) {
+        boolean iguales=false;
+
+        if(this==obj)
+            iguales=true;
+        else{
+            if(obj instanceof Afiliado){ //instanceof ya valora si el objeto es nulo
+                if(this.getIdentificador()==((Afiliado) obj).getIdentificador())
+                    iguales=true;
+            }
+        }
+
+        return iguales;
+    }*/
+
+}
